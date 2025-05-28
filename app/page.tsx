60023723a8f40a951fc8c1f62393ee6205eaaf23
Ng Wei Zong Damian <damianng87@gmail.com>
2025-05-28 16:17:50 +0800

    redirect dashboard after login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image';
 import { NavBar } from './components/NavBar';
+import { auth } from './lib/auth';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-return (
+export default async function Home() {
+  const session = await auth();
+  
+  if (session?.user) {
+    return redirect('/dashboard');
+  }
+  
+  return (
     <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
       <NavBar />
     </div>
