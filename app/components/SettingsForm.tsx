a8864a8b190c8cb25cedc8512f6ffb7adbd6a3a7
Ng Wei Zong Damian <damianng87@gmail.com>
2025-06-11 13:38:30 +0800

    minor changes

--- app/components/SettingsForm.tsx
+++ app/components/SettingsForm.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SubmitButton } from "./SubmitButtons";
@@ -15,97 +22,108 @@ import { UploadDropzone } from "../lib/uploadthing";
 import { toast } from "sonner";
 
 interface iAppProps {
-    fullName: string;
-    email: string;
-    profileImage: string;
+  fullName: string;
+  email: string;
+  profileImage: string;
 }
 
+const DEFAULT_AVATAR = "/defaultAvatar.png";
+
 export function SettingsForm({ email, fullName, profileImage }: iAppProps) {
-    const [lastResult, action] = useActionState(SettingsAction, undefined);
-    const [currentProfileImage, setCurrentProfileImage] = useState(profileImage);
-    const [form, fields] = useForm({
-        lastResult,
+  const [lastResult, action] = useActionState(SettingsAction, undefined);
+  const [currentProfileImage, setCurrentProfileImage] = useState(profileImage);
+  const [form, fields] = useForm({
+    lastResult,
 
-        onValidate({formData}) {
-            return parseWithZod(formData, {
-                schema: settingsScheme,
-            });
-        },
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: settingsScheme,
+      });
+    },
 
-        shouldValidate: "onBlur",
-        shouldRevalidate: "onInput"
-    });
-    const handleDeleteImage = () => {
-        setCurrentProfileImage("");
-    }
-    
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Settings</CardTitle>
-                <CardDescription>Manage your account settings.</CardDescription>
-            </CardHeader>
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
+  const handleDeleteImage = () => {
+    setCurrentProfileImage("");
+  };
 
-            <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
-                <CardContent className="flex flex-col gap-y-4">
-                    <div className="flex flex-col gap-y-2">
-                        <Label>Full Name</Label>
-                        <Input name={fields.fullName.name} key={fields.fullName.key} defaultValue={fullName} placeholder="John Doe" />
-                        <p className="text-red-500 text-sm">{fields.fullName.errors}</p>
-                    </div>
-                    <div className="flex flex-col gap-y-2">
-                        <Label>Email</Label>
-                        <Input disabled defaultValue={email} placeholder="abc@gmail.com" />
-                    </div>
-                    <div className="grid gap y-2">
-                        <Label>Profile Image</Label>
-                        <input 
-                            type="hidden" 
-                            name={fields.profileImage.name} 
-                            key={fields.profileImage.key} 
-                            value={currentProfileImage} 
-                        />
-                        <div style={{ height: '1rem' }} />
-                        {currentProfileImage ? (
-                                <div className="relative size-16">
-                                    <img 
-                                        src={currentProfileImage}
-                                        alt="Profile Image"
-                                        className="size-16 rounded-lg"
-                                    />
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Settings</CardTitle>
+        <CardDescription>Manage your account settings.</CardDescription>
+      </CardHeader>
 
-                                    <Button 
-                                        onClick={handleDeleteImage}
-                                        variant="destructive"  
-                                        size="icon"
-                                        type="button" 
-                                        className="absolute -top-3 -right-3"
-                                    >
-                                        <X className="size-3" />
-                                    </Button>
-                                </div>
-                            ) : (
-                                <UploadDropzone 
-                                    onClientUploadComplete={(res) => {
-                                        setCurrentProfileImage(res[0].url)
-                                        toast.success("Profile image uploaded!")
-                                    }} 
-                                    onUploadError={(error) => {
-                                        console.log("something went wrong", error)
-                                        toast.error(error.message)
-                                    }}
-                                    endpoint="imageUploader" 
-                                />
-                            )
-                        }
-                        <p className="text-red-500 text-sm">{fields.profileImage.errors}</p>
-                    </div>
-                </CardContent>
-                <div style={{ height: '1rem' }} />
-                <CardFooter>
-                    <SubmitButton text="Save Changes" />
-                </CardFooter>
-            </form>
-        </Card>  
-    );
+      <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
+        <CardContent className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-y-2">
+            <Label>Full Name</Label>
+            <Input
+              name={fields.fullName.name}
+              key={fields.fullName.key}
+              defaultValue={fullName}
+              placeholder="John Doe"
+            />
+            <p className="text-red-500 text-sm">{fields.fullName.errors}</p>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Email</Label>
+            <Input disabled defaultValue={email} placeholder="abc@gmail.com" />
+          </div>
+          <div className="grid gap y-2">
+            <Label>Profile Image</Label>
+            <input
+              type="hidden"
+              name={fields.profileImage.name}
+              key={fields.profileImage.key}
+              value={currentProfileImage}
+            />
+            <div style={{ height: "1rem" }} />
+            {currentProfileImage ? (
+              <div className="relative size-16">
+                <img
+                  src={currentProfileImage}
+                  alt="Profile Image"
+                  className="size-16 rounded-lg"
+                  onError={(e) => {
+                    const target = e.target as HTMLImageElement;
+                    target.onerror = null;
+                    target.src = DEFAULT_AVATAR;
+                  }}
+                />
+
+                <Button
+                  onClick={handleDeleteImage}
+                  variant="destructive"
+                  size="icon"
+                  type="button"
+                  className="absolute -top-3 -right-3"
+                >
+                  <X className="size-3" />
+                </Button>
+              </div>
+            ) : (
+              <UploadDropzone
+                onClientUploadComplete={(res) => {
+                  setCurrentProfileImage(res[0].url);
+                  toast.success("Profile image uploaded!");
+                }}
+                onUploadError={(error) => {
+                  console.log("something went wrong", error);
+                  toast.error(error.message);
+                }}
+                endpoint="imageUploader"
+              />
+            )}
+            <p className="text-red-500 text-sm">{fields.profileImage.errors}</p>
+          </div>
+        </CardContent>
+        <div style={{ height: "1rem" }} />
+        <CardFooter>
+          <SubmitButton text="Save Changes" />
+        </CardFooter>
+      </form>
+    </Card>
+  );
 }
