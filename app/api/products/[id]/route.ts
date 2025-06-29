84c6cb425e75d6b40c8785db33cf173c76d5fe22
Ng Wei Zong Damian <damianng87@gmail.com>
2025-06-30 00:50:20 +0800

    deploy 3

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -4,7 +4,7 @@ import { isLoggedIn } from "@/app/lib/hooks";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id: productId } = await params;
