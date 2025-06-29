2d78477a0a090a9773cb1684c4d6c1a7f76b5097
Ng Wei Zong Damian <damianng87@gmail.com>
2025-06-30 01:15:26 +0800

    image use normal route instead

--- app/onboarding/grant-id/page.tsx
+++ app/onboarding/grant-id/page.tsx
@@ -1,28 +1,39 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import VideoGif from '@/public/LoadingGif.gif';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { CalendarCheckIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function OnboardingrouteTwo() {
-    return (
-        <div className="min-h-screen w-screen flex items-center justify-center">
-            <Card>
-                <CardHeader>
-                    <CardTitle>You are almost finished!</CardTitle>
-                    <CardDescription>We have to now connect your calendar to your account.</CardDescription>
-                    <Image src={VideoGif} alt="Almost finished gif" className="w-full rounded-lg"/>
-                </CardHeader>
-                <CardContent>
-                    <Button asChild className="w-full">
-                        <div>
-                            <CalendarCheckIcon className="size-4 mr-2" />
-                            <Link href="/api/auth">Connect Calendar to your Account</Link>
-                        </div>
-                    </Button>
-                </CardContent>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>You are almost finished!</CardTitle>
+          <CardDescription>
+            We have to now connect your calendar to your account.
+          </CardDescription>
+          <Image
+            src="/LoadingGif.gif"
+            alt="Almost finished gif"
+            className="w-full rounded-lg"
+          />
+        </CardHeader>
+        <CardContent>
+          <Button asChild className="w-full">
+            <div>
+              <CalendarCheckIcon className="size-4 mr-2" />
+              <Link href="/api/auth">Connect Calendar to your Account</Link>
+            </div>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
